95fbc458425bfe27f1308e8c866cf5744ab7445d
trishitha22 <trishitha2206@gmail.com>
2025-09-24 15:16:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return 'Hello Trishithaaaaaaaaaa'
+    return 'Hello Trishitha'
+    #type somethinggggg
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
